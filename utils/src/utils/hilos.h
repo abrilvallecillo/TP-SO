7f32aed11a9c8c528c08ced197d5638c11c5bd8b
abrilvallecillo <avallecillo@frba.utn.edu.ar>
2024-07-11 20:01:09 -0300

    Delete utils directory

--- utils/src/utils/hilos.h
+++
@@ -1,9 +0,0 @@
-#ifndef UTILS_HILOS_H_
-#define UTILS_HILOS_H_
-
-#include <pthread.h>
-
-pthread_t crearHilo(void *(*funcionAUsar)(void *), void * parametroDeFuncion);
-
-void crearMutex(pthread_mutex_t mutex);
-#endif
